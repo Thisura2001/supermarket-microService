73ca19efdf241fdaa02412100cbe35776939827a
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-02-18 11:40:04 +0530

    add order service

---
+++ Product-service/src/main/java/com/example/productservice/Controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.productservice.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @GetMapping("/get")
+    public String getProduct() {
+        return "Product";
+    }
+}
